6831eb8d1cbb53661484c56715dfdba40d67d79f
Vlad Deev <vlad.deev@gmail.com>
2017-04-18 15:43:10 -0400

    AUTH: Complete Users and Sessions Controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,23 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+                                      params[:user][:email],
+                                      params[:user][:password]
+                                    )
+    if @user
+      log_in!(@user)
+      render "api/users/show"
+    else
+      render json: { base: ["Invalid email/password combination"] }, status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      log_out!
+      render json: {}
+    else
+      render json: { base: ["You are already signed out"] }, status: 418
+    end
+  end
 end
