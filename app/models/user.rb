e26de53d6c120948ae2c77e340c204772597c8cc
Vlad Deev <vlad.deev@gmail.com>
2017-04-18 15:26:27 -0400

    AUTH: Set backend structure

---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ActiveRecord::Base
+  before_save { email.downcase! }
+  validates :first_name, :last_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_pasword?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_pasword?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+end
