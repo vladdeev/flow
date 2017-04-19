95fa2f8266bbb6a78f063fc10afaabd0c6fae06b
Vlad Deev <vlad.deev@gmail.com>
2017-04-19 14:50:16 -0400

    AUTH: Fixed bug with current user

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-
-
-
 import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 
-
 const Root = ({ store }) => {
-
   const _ensureLoggedIn = (nextState, replace) => {
     const currentUser = store.getState().session.currentUser;
     if (!currentUser) {
--- frontend/flow.jsx
+++ frontend/flow.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store'
-import Root from './components/root'
+import configureStore from './store/store';
+import Root from './components/root';
+import { logout } from './actions/session_actions';
 
+window.logout = logout;
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -12,6 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
   window.store = store;
-  const root = document.getElementById('root')
+  const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
