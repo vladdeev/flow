b266f713a2fba55ac09a58bd7a7b115de1412159
Vlad Deev <vlad.deev@gmail.com>
2017-04-19 10:34:50 -0400

    AUTH: Complete store, app and root components, update entry file

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::UsersController < ApplicationController
   def create
-    debugger
     @user = User.new(user_params)
     if @user.save
       log_in!(@user)
--- frontend/flow.jsx
+++ frontend/flow.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as SessionAPIUtil from './util/session_api_util'
+import configureStore from './store/store'
+import Root from './components/root'
 
-window.login = SessionAPIUtil.login;
-window.logout = SessionAPIUtil.logout;
-window.signup = SessionAPIUtil.signup;
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root')
-  ReactDOM.render(<h1>Welcome to Flow</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
