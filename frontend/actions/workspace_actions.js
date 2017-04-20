0de0876b1d93c74129a69bc08ac740c982128fe2
Vlad Deev <vlad.deev@gmail.com>
2017-04-20 17:59:30 -0400

    WS: Add workspace actions

---
+++ frontend/actions/workspace_actions.js
@@ -0,0 +1,56 @@
+import * as workspaceAPIUtil from '../util/workspace_api_util';
+
+export const RECEIVE_ALL_WORKSPACES = "RECEIVE_ALL_WORKSPACES";
+export const RECEIVE_WORKSPACE = "RECEIVE_WORKSPACE";
+export const REMOVE_WORKSPACE = "REMOVE_WORKSPACE";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+const receiveAllWorkspaces = (workspaces) => ({
+  type: RECEIVE_ALL_WORKSPACES,
+  workspaces
+});
+
+const receiveWorkspace = (workspace) => ({
+  type: RECEIVE_WORKSPACE,
+  workspace
+});
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+const removeWorkspace = (workspace) => ({
+  type: REMOVE_WORKSPACE,
+  workspaceId: workspace.id
+});
+
+export const fetchAllWorkspaces = () => dispatch => (
+  workspaceAPIUtil.fetchAllWorkspaces()
+    .then(workspaces => dispatch(receiveAllWorkspaces(workspaces)),
+      err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const fetchWorkspace = id => dispatch (
+  workspaceAPIUtil.fetchWorkspace(id)
+    .then(workspace => dispatch(receiveWorkspace(workspace)),
+      err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const createWorkspace = workspace => dispatch (
+  workspaceAPIUtil.createWorkspace(workspace)
+    .then(workspace => dispatch(receiveWorkspace(workspace)),
+      err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const updateWorkspace = workspace => dispatch (
+  workspaceAPIUtil.updateWorkspace(workspace)
+    .then(workspace => dispatch(receiveWorkspace(workspace)),
+      err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const deleteWorkspace = workspace => dispatch (
+  workspaceAPIUtil.deleteWorkspace(workspace)
+    .then(workspace => dispatch(removeWorkspace(workspace)),
+      err => dispatch(receiveErrors(err.responseJSON)))
+);
--- frontend/flow.jsx
+++ frontend/flow.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import { logout } from './actions/session_actions';
-import * as workspaceAPIUtil from './util/workspace_api_util'
+// import * as workspaceAPIUtil from './util/workspace_api_util'
+//
+// window.logout = logout;
+// window.fetchAllWorkspaces = workspaceAPIUtil.fetchAllWorkspaces;
+// window.fetchWorkspace = workspaceAPIUtil.fetchWorkspace;
+// window.createWorkspace = workspaceAPIUtil.createWorkspace;
+// window.updateWorkspace = workspaceAPIUtil.updateWorkspace;
+// window.deleteWorkspace = workspaceAPIUtil.deleteWorkspace;
 
-window.logout = logout;
-window.fetchAllWorkspaces = workspaceAPIUtil.fetchAllWorkspaces;
-window.fetchWorkspace = workspaceAPIUtil.fetchWorkspace;
-window.createWorkspace = workspaceAPIUtil.createWorkspace;
-window.updateWorkspace = workspaceAPIUtil.updateWorkspace;
-window.deleteWorkspace = workspaceAPIUtil.deleteWorkspace;
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -24,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  window.store = store;
+  // window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
