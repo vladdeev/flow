36b7166245cd9dc39d47d5846df90c59754b76bf
Vlad Deev <vlad.deev@gmail.com>
2017-04-24 15:19:39 -0400

    Complete front page

--- frontend/components/app/front_page.jsx
+++ frontend/components/app/front_page.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import HeaderContainer from '../header/header_container';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
+
+const handleClick = () => e => {
+  e.preventDefault();
+  hashHistory.push('/signup');
+}
 
 const FrontPage = (props) => (
   <div>
     <HeaderContainer />
     <div className='front-container'>
       <div className="first-screen">
+        <h1>Get the job done</h1>
+        <h2>Flow is the best way to get your team work togeter</h2>
+        <h3 onClick={handleClick()}>Join Flow for free</h3>
       </div>
     </div>
   </div>
